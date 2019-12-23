8c017da3cc8ed2b5ecb3cccf2d6f630fe6eede5d
Limacher <alexis.limacher@hach.com>
2019-12-23 09:17:57 +0100

    testtt

--- Dalle_Detect_main 1.1.3.py
+++ Dalle_Detect_main 1.1.3.py
@@ -6,7 +6,7 @@
 
 import pygame, os, re
 os.chdir("C:\\Users\\alimacher\\Desktop\\Work\\1ere annee\\Python\\PyGame\\Dalle_Detect")
-#os.chdir("C:\\Users\\alexi\\Desktop\\GIT\\CD4500-TouchScreen-1")
+##os.chdir("C:\\Users\\alexi\\Desktop\\GIT\\CD4500-TouchScreen-1")
 del os
 
 #INITIALISATION
